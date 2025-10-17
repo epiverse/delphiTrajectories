847c29bf4fbfa3f49c3fec57c35a6910abdf0b2c
Jonas Almeida <jalmeida@mathbiol.org>
2025-10-17 14:27:37 -0400

    generated trajectory

---
+++ funDelphi.mjs
@@ -0,0 +1,89 @@
+let eventsList_start = [
+    {
+        "event": "Male",
+        "age": 0
+    },
+    {
+        "event": "B01 Varicella [chickenpox]",
+        "age": 2
+    },
+    {
+        "event": "L20 Atopic dermatitis",
+        "age": 3
+    },
+    {
+        "event": "No event",
+        "age": 5
+    },
+    {
+        "event": "No event",
+        "age": 10
+    },
+    {
+        "event": "No event",
+        "age": 15
+    },
+    {
+        "event": "No event",
+        "age": 20
+    },
+    {
+        "event": "G43 Migraine",
+        "age": 20
+    },
+    {
+        "event": "E73 Lactose intolerance",
+        "age": 21
+    },
+    {
+        "event": "B27 Infectious mononucleosis",
+        "age": 22
+    },
+    {
+        "event": "No event",
+        "age": 25
+    },
+    {
+        "event": "J11 Influenza, virus not identified",
+        "age": 28
+    },
+    {
+        "event": "No event",
+        "age": 30
+    },
+    {
+        "event": "No event",
+        "age": 35
+    },
+    {
+        "event": "No event",
+        "age": 40
+    },
+    {
+        "event": "Smoking low",
+        "age": 41
+    },
+    {
+        "event": "BMI mid",
+        "age": 41
+    },
+    {
+        "event": "Alcohol low",
+        "age": 41
+    },
+    {
+        "event": "No event",
+        "age": 42
+    }
+]
+
+async function generatedTrajectory(eventsList=eventsList_start,seed=42){ // note default values
+    const { generateTrajectory } = await import("https://episphere.github.io/delphi-onnx/delphiSDK.js")
+    return await generateTrajectory({eventsList, seed: 42})
+}
+
+export{generatedTrajectory}
+
+// NOTES
+// trj = await (await import('epiverse.github.io/delphiTrajectories/funDelphi.mjs')).generatedTrajectory()
+// trj = await (await import('http://localhost:8000/delphiTrajectories/funDelphi.mjs')).generatedTrajectory()
